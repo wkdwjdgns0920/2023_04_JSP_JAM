0d5adad5150d962d6eac75666ec76f63777995b7
wkdwjdgns0920 <wkdwjdgns0920@gmail.com>
2023-04-06 14:39:55 +0900

    구구단

--- src/main/java/com/KoreaIT/java/jam/HomeMainServlet3.java
+++ src/main/java/com/KoreaIT/java/jam/HomeMainServlet3.java
@@ -7,11 +7,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/home/main")
-public class HomeMainServlet extends HttpServlet {
+@WebServlet("/home/main2")
+public class HomeMainServlet3 extends HttpServlet {
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Hello World!").append(request.getContextPath());
+		response.getWriter().append("Hello World!!!!!!!!!!!!!!!!!!!").append(request.getContextPath());
 	}
 
 }
