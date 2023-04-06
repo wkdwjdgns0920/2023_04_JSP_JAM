955a6106cc1358290434319f64e49a854eb9cf30
wkdwjdgns0920 <wkdwjdgns0920@gmail.com>
2023-04-06 13:48:02 +0900

    서블릿생성

---
+++ src/main/java/com/KoreaIT/java/jam/HomeMainServlet.java
@@ -0,0 +1,20 @@
+package com.KoreaIT.java.jam;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/home/main2")
+public class HomeMainServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("HelloWorld22dsnakgsdnikon").append(request.getContextPath());
+	}
+
+
+}
---
+++ src/main/java/com/KoreaIT/java/jam/HomeMainServlet2.java
@@ -0,0 +1,20 @@
+package com.KoreaIT.java.jam;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/home/main")
+public class HomeMainServlet2 extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("HelloWorld").append(request.getContextPath());
+	}
+
+
+}
