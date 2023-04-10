06bd2d56acc3e7249caaaf20bec9328c97dd175c
wkdwjdgns0920 <wkdwjdgns0920@gmail.com>
2023-04-10 17:21:53 +0900

    detail.jsp 에 수정, 삭제 추가

--- src/main/java/com/KoreaIT/java/jam/servlet/ArticleDoWriteServlet.java
+++ src/main/java/com/KoreaIT/java/jam/servlet/ArticleDoWriteServlet.java
@@ -39,7 +39,9 @@ public class ArticleDoWriteServlet extends HttpServlet {
 
 		try {
 			conn = DriverManager.getConnection(url, user, password);
-
+			
+			request.setCharacterEncoding("UTF-8");
+			
 			String title = request.getParameter("title");
 			String body = request.getParameter("body");
 
